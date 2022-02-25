0af6c49163815f0240f071c9f8e1e50ebf6d0aae
hduprat <hugod@bam.tech>
2022-02-25 15:36:43 +0100

    :sparkles: (App) load Mytupi font

--- App.tsx
+++ App.tsx
@@ -1,7 +1,14 @@
+import AppLoading from "expo-app-loading";
+import { useFonts } from "expo-font";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [hasBeenLoaded] = useFonts({
+    Mytupi: require("./assets/fonts/mytupi.ttf"),
+  });
+
+  if (!hasBeenLoaded) return <AppLoading />;
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
